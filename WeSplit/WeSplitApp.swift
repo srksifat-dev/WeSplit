43c35120adbbe34d60001cd5fc917314c0ad2288
srksifat-dev <srksifat.dev@gmail.com>
2024-09-29 18:39:46 +0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Md. Shoibur Rahman Khan Sifat on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
